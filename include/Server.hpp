1b6e7bf7909825354e638e37342c7ee132d528e0
aken20 <teentaiten@gmail.com>
2025-02-14 04:43:26 +0400

    fixing the Server.cpp conflict

--- include/Server.hpp
+++ include/Server.hpp
@@ -41,7 +41,7 @@ private:
     std::string _password;
     int _serverSocket;
     std::vector<Client> Clients;
-    std::vector<Channel> Channeils;
+    std::vector<Channel> Channels;
     struct sockaddr_in _server_addr;
     Parser _parser;
     Commands _commands;
